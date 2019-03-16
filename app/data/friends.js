1bbfa7898c053ea93103ecc765835c4e4fa0a0dc
Vince Charals <vincecharals@gmail.com>
2019-03-15 22:40:37 -0500

    -finished hw

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'James Panther',
+		photo: 'hhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvzgOr7SlLww8LIMrRaE1ZlZ6UdGA6hKdcgjr8ucMjC7IaExl2KQ',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Tory Batty',
+		photo: 'https://www.sideshow.com/product-asset/9032461/feature',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Venom Thompson',
+		photo: 'https://cdn3-www.comingsoon.net/assets/uploads/2018/09/VLC-00009.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Iron Hackman',
+		photo: 'https://target.scene7.com/is/image/Target/GUEST_6edda33b-ae7c-4d5f-b4f7-47065d935264?wid=488&hei=488&fmt=pjpeg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Kakarot Charals',
+		photo: 'http://dessinbebe.club/wp-content/uploads/2018/09/goku-super-saiyan-4manuchaann-on-deviantart-destine-sangoku-super-saiyan-4.png',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Carnage Mcgee',
+		photo: 'https://www.sideshow.com/storage/product-images/300467/carnage_marvel_gallery_5c4d74a411f78.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Shawn Pool',
+		photo: 'https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/05/Deadpool-920x584.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Logan Blades',
+		photo: 'https://www.syfy.com/sites/syfy/files/styles/1200x680/public/wire/legacy/Hugh-Jackman-Wolverine_0.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jack Big Cat',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGzBqYADlSpIB1mhObj80iB7gojGRAGWloOd87Cr7pkwvhZlf3hQ',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Super Grover Lee',
+		photo: 'https://vignette.wikia.nocookie.net/muppet/images/e/e8/SGpose.jpg/revision/latest/scale-to-width-down/316?cb=20121110170149',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
